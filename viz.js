7dfe411ea8728a30c0d600dfcab084189560ce55
Matteo Subet <matteo.subet@student.supsi.ch>
2023-06-16 10:00:59 +0200

    JSON fix error on viz.js

--- viz.js
+++ viz.js
@@ -11,6 +11,27 @@
 
 // Code based on: array of bouncing balls with vectors in a class by 2sman
 
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("ballData.json")
+      .then(response => response.json())
+      .then(data => renderData(data))
+      .catch(error => console.error(error));
+});
+
+function renderData(data) {
+  console.log("File " + data.title + " uploaded!");
+  let abDimension = [];
+  let abSpeedX = [];
+  let abSpeedY = [];
+  let abColor = [];
+  data.items.forEach(item => {
+      abDimension.push(item.dimension);
+      abSpeedX.push(item.speedX);
+      abSpeedY.push(item.speedY);
+      abColor.push(item.color);
+  });
+}
+
 let makeawarePeople = [];
 var numBalls = 23;
 let colors = ["#FF3A17", "#ABABAB", "#FF46ED"];
