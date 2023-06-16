281f4e287a8ef5d2b02d1aaeafd33f7661832ddf
Matteo Subet <matteo.subet@student.supsi.ch>
2023-06-16 10:03:33 +0200

    fix on the JSON file – variables created inside a function will not be readed on another js file

--- json.js
+++ json.js
@@ -1,22 +1,26 @@
-
+let abDimension = [];
+let abSpeedX = [];
+let abSpeedY = [];
+let abColor = [];
 
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("ballData.json")
-        .then(response => response.json())
-        .then(data => renderData(data))
-        .catch(error => console.error(error));
+  fetch("ballData.json")
+      .then(response => response.json())
+      .then(data => renderData(data))
+      .catch(error => console.error(error));
 });
 
 function renderData(data) {
-    console.log("File " + data.title + " uploaded!");
-    let abDimension = [];
-    let abSpeedX = [];
-    let abSpeedY = [];
-    let abColor = [];
-    data.items.forEach(item => {
-        abDimension.push(item.dimension);
-        abSpeedX.push(item.speedX);
-        abSpeedY.push(item.speedY);
-        abColor.push(item.color);
-    });
+  console.log("File " + data.title + " uploaded!");
+  abDimension = [];
+  abSpeedX = [];
+  abSpeedY = [];
+  abColor = [];
+  
+  data.items.forEach(item => {
+      abDimension.push(item.dimension);
+      abSpeedX.push(item.speedX);
+      abSpeedY.push(item.speedY);
+      abColor.push(item.color);
+  });
 }
